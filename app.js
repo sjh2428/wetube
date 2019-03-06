7d2fab7cc74e97d58ef5d27c66e62b73f91bb7dd
sjh2428 <pok_gare@naver.com>
2019-03-06 11:15:33 +0900

    simple V complete

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes"
+import { localsMiddleWare } from "./middlewares";
+const app = express();
+
+app.use(helmet());
+app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.use(localsMiddleWare)
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+// const betweenHome = (req, res, next) => {
+//     console.log("Between");
+//     next();
+// };
+
+// app.use(betweenHome);
+
+//const middleware = (req, res, next) => {
+//    res.send("not happening");
+//};  //Like this, middleware can cut the connection.
+//app.get("/", betweenHome, handleHome);
+
+export default app;
---
+++ routers/videoRouter.js
@@ -0,0 +1,13 @@
+import express from "express";
+import routes from "../routes";
+import { deleteVideo, editVideo, videoDetail, upload, videos } from "../controller/videoController";
+
+const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
+
+export default videoRouter;
